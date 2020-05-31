a8ac338979e83669f3a9ea061fe73da35b2560f3
Michael Schramm <micha.schramm@gmail.com>
2020-06-01 01:59:58 +0200

    fix variable passing for jwt strategy

--- src/service/auth/jwt.strategy.ts
+++ src/service/auth/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('AUTH_SECRET'),
+      secretOrKey: configService.get<string>('SECRET_KEY'),
     });
   }
 
