92261e78e49e95c46754742e99c2ccf2422f5485
wodka <michael.schramm@gmail.com>
2020-06-20 08:55:29 +0200

    fix typo

--- src/service/submission/index.ts
+++ src/service/submission/index.ts
@@ -3,7 +3,7 @@ import { SubmissionSetFieldService } from './submission.set.field.service';
 import { SubmissionStartService } from './submission.start.service';
 import { SubmissionStatisticService } from './submission.statistic.service';
 import { SubmissionTokenService } from './submission.token.service';
-import { SubmissionHookService } from './submission.webhook.service'
+import { SubmissionHookService } from './submission.hook.service'
 
 export const submissionServices = [
   SubmissionService,
